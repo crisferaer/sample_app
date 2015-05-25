a9fddcb54bd034d32b4e4e45503c687dc450ccbd
crisferaer <crisferaer@example.com>
2015-05-25 08:12:58 +0800

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -64,8 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
 
     #Applicication.rb
     config.assets.initialize_on_precompile = false
+  end
 end
