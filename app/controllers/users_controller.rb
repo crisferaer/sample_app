c4da0ef1377009ccea4edaffb814d1500c6653ff
crisferaer <crisferaer@example.com>
2015-05-29 11:32:29 +0800

    Completed signin/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
+
+    # json stuff
+    #respond_to do |format|
+    #  format.html
+    #  format.json { render json: @user, :only => [:email, :name],
+    #           :include => "hello" }
+    #end
   end
   
   def new
@@ -13,6 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       redirect_to @user, :flash => {:success => "Welcome to the Sample App!"}
     else
       @title = "Sign up"
